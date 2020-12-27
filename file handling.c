0f2bdb754296ac10e6c93cfcef3ba53eb368578c
Aartichoudhary41 <74286612+Aartichoudhary41@users.noreply.github.com>
2020-12-27 19:57:17 +0530

    Add files via upload

---
+++ file handling.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	//1. declare the FILE pointer
+	
+	//2. open - Read, Write and Append
+	
+	//3. Read content from the file 
+	
+	//4. write content to the file 
+	
+	//5. Append content to the file
+	
+	//6.Close the pointer 
+}
